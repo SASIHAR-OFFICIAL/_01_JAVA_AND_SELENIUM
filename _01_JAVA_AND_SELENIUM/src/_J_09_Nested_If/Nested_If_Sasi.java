de654d9abb3f90f324581b720cf6892716091ce6
SASIHAR <SASIHAR@SASIHAR-PC>
2025-02-20 23:28:51 +0530

    20 FEB 25

---
+++ _01_JAVA_AND_SELENIUM/src/_J_09_Nested_If/Nested_If_Sasi.java
@@ -0,0 +1,25 @@
+package _J_09_Nested_If;
+
+import java.util.Scanner;
+
+public class Nested_If_Sasi {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int input1 = scan.nextInt();
+		if (input1 <=1) {
+			System.out.println("Type 2");
+			int input2 = scan.nextInt();
+			if (input2<=2) {
+				System.out.println("Type 3");
+				int input3 = scan.nextInt();
+				if (input3 <=3) {
+					System.out.println("Type 4");
+				}
+
+			}
+		}
+	}
+
+}
