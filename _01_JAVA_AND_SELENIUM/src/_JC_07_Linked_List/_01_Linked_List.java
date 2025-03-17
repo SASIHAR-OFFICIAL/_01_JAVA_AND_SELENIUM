d4047b745ba5ba5e1b0cdb635a823661a84e0fa4
SASIHAR <SASIHAR@SASIHAR-PC>
2025-03-17 22:35:20 +0530

    17 MARCH 2025 FROM HOME

--- _01_JAVA_AND_SELENIUM/src/_JC_07_Linked_List/_01_Linked_List.java
+++ _01_JAVA_AND_SELENIUM/src/_JC_07_Linked_List/_01_Linked_List.java
@@ -1,9 +1,16 @@
 package _JC_07_Linked_List;
 
+import java.util.LinkedList;
+
 public class _01_Linked_List {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList LL1 = new LinkedList();
+		LL1.add(1);
+		LL1.add(16);
+		LL1.add(22);
+		LL1.add("SASI");
+		System.out.println(LL1);
 
 	}
 
