c7dedf2706ce2f89cf4120110add3e008fba9168
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2025-04-17 16:06:43 +0530

    17 APRIL 25 FROM OFFICE

--- _01_JAVA_AND_SELENIUM/src/_S_05_18_Upload_File/Upload_Robot_14_APRIL_25.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_18_Upload_File/Upload_Robot_14_APRIL_25.java
@@ -11,6 +11,7 @@ import org.bouncycastle.oer.its.ieee1609dot2.VerificationKeyIndicator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Upload_Robot_14_APRIL_25 {
 
@@ -18,30 +19,40 @@ public class Upload_Robot_14_APRIL_25 {
 		System.setProperty("Webdriver.Gecko.Driver", "C:\\WebDriver\\geckodriver\\geckodriver.exe");
 		FirefoxDriver d = new FirefoxDriver();
 		d.navigate().to("https://practice.expandtesting.com/upload");
-		
-		WebElement Upload_button = d.findElement(By.xpath("//*[@id=\"fileInput\"]"));
-		//*[@id="fileInput"]
-		Upload_button.click();
-		
-		String File_Upload = "D:\\sele\\JavaFileWrite2.txt";
-		StringSelection File_name = new StringSelection(File_Upload);
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(File_name, null);
-		
-		Robot Robot_obj = new Robot();
-		Robot_obj.keyPress(KeyEvent.VK_CONTROL);
-		Robot_obj.keyPress(KeyEvent.VK_C);
-		Robot_obj.keyPress(KeyEvent.VK_CONTROL);
-		Robot_obj.keyPress(KeyEvent.VK_V);
-		Robot_obj.keyRelease(KeyEvent.VK_V);
-		Robot_obj.keyRelease(KeyEvent.VK_CONTROL);
-		Robot_obj.keyRelease(KeyEvent.VK_C);
-		Robot_obj.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+
+
+		WebElement Upload_button = d.findElement(By.xpath("//input[@id='fileInput'][@class='form-control']"));
 		Thread.sleep(3000);
-		Robot_obj.keyPress(KeyEvent.VK_ENTER);
-		Thread.sleep(3000);
-		System.out.println("Uploaded");
-	
-//		Virtual_Keyboard.keyPress(KeyEvent.VK_CONTROL);
+		
+		Actions Action_click  = new Actions(d);
+		Action_click.clickAndHold(Upload_button);
+		
+//		Robot Robot_obj1 = new Robot();
+//		Robot_obj1.keyPress(KeyEvent.VK_ENTER);
+//		Robot_obj1.keyRelease(KeyEvent.VK_ENTER);
+		
+		
+
+//		String File_Upload = "D:\\sele\\JavaFileWrite2.txt";
+//		StringSelection File_name = new StringSelection(File_Upload);
+//		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(File_name, null);
+//
+//		Robot Robot_obj = new Robot();
+//		Robot_obj.keyPress(KeyEvent.VK_CONTROL);
+//		Robot_obj.keyPress(KeyEvent.VK_C);
+//		Robot_obj.keyPress(KeyEvent.VK_CONTROL);
+//		Robot_obj.keyPress(KeyEvent.VK_V);
+//		Robot_obj.keyRelease(KeyEvent.VK_V);
+//		Robot_obj.keyRelease(KeyEvent.VK_CONTROL);
+//		Robot_obj.keyRelease(KeyEvent.VK_C);
+//		Robot_obj.keyRelease(KeyEvent.VK_CONTROL);
+//		Thread.sleep(3000);
+//		Robot_obj.keyPress(KeyEvent.VK_ENTER);
+//		Thread.sleep(3000);
+//		System.out.println("Uploaded");
+
+		//		Virtual_Keyboard.keyPress(KeyEvent.VK_CONTROL);
 	}
 
 }
