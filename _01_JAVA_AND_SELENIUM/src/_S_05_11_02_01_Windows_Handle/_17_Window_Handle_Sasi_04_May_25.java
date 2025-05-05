cf36fe8424c63273a3af3fce46697cad470677f8
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2025-05-05 16:23:49 +0530

    05 May 2025 from office

---
+++ _01_JAVA_AND_SELENIUM/src/_S_05_11_02_01_Windows_Handle/_17_Window_Handle_Sasi_04_May_25.java
@@ -0,0 +1,49 @@
+package _S_05_11_02_01_Windows_Handle;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class _17_Window_Handle_Sasi_04_May_25 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("Webdriver.Gecko.Driver", "C:\\WebDriver\\geckodriver\\geckodriver.exe");
+		FirefoxDriver d = new FirefoxDriver();
+		d.navigate().to("https://letcode.in/window");
+		String Parent_Window = d.getWindowHandle();
+
+		WebElement Open_Home_Page = d.findElement(By.xpath("//*[text()='Open Home Page']"));
+		Open_Home_Page.click();
+		Set<String> Window_2 = d.getWindowHandles();
+
+		String After_Click = d.getCurrentUrl();
+		System.out.println("After_Click = "+After_Click);
+
+		List<String> Window_2_list = new ArrayList<>(Window_2);
+		int Window_2_size = Window_2_list.size();
+		System.out.println("Window_2_size = "+Window_2_size);
+		d.switchTo().window(Window_2_list.get(1));
+		Thread.sleep(3000);
+		String currentUrl = d.getCurrentUrl();
+		System.out.println("currentUrl ="+currentUrl);
+		d.switchTo().window(Window_2_list.get(0));
+		Window_2_list.clear();
+
+		WebElement Muiltiple_window = d.findElement(By.xpath("//*[text()='Muiltiple windows']"));
+		Muiltiple_window.click();
+
+		Set<String> windowHandles = d.getWindowHandles();
+
+		int size = windowHandles.size();
+		System.out.println("size ="+size);
+		List<String> Multi_Win = new ArrayList<>();
+		for (String string : Multi_Win) {
+			d.switchTo().newWindow(string.indexOf(size));
+			System.out.println("title ="+title);
+		}
+		}	}
