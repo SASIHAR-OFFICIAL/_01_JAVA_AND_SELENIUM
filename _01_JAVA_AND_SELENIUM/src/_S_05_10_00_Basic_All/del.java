d4b1c2392eab876271bc343fcc3a9b10fe4be4e0
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2025-05-24 16:33:49 +0530

    24 MAY 2025 FROM OFFICE

---
+++ _01_JAVA_AND_SELENIUM/src/_S_05_10_00_Basic_All/del.java
@@ -0,0 +1,14 @@
+package _S_05_10_00_Basic_All;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class del {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+	driver.navigate()
+
+	}
+
+}
