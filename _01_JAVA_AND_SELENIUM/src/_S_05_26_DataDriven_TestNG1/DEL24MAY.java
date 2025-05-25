8dd1ca44c448fdaa8583fb8406436b8fa025f427
SASIHAR <SASIHAR@SASIHAR-PC>
2025-05-25 23:52:09 +0530

    25 MAY 2025 DROM HOME

--- _01_JAVA_AND_SELENIUM/src/_S_05_26_DataDriven_TestNG1/DEL24MAY.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_26_DataDriven_TestNG1/DEL24MAY.java
@@ -1,14 +1,13 @@
-package _S_05_10_00_Basic_All;
+package _S_05_26_DataDriven_TestNG1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class del {
+public class DEL24MAY {
 
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
-	driver.n
-
+		driver.navigate().to("https://www.google.com");
 	}
 
 }
