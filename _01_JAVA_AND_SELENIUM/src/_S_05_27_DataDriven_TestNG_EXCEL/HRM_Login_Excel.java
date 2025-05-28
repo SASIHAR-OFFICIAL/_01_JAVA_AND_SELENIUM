d4cc8838ed60a09524049b78c4f2da30e5103b8f
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2025-05-28 16:20:52 +0530

    28 MAY 2025 FROM OFFICE

--- _01_JAVA_AND_SELENIUM/src/_S_05_27_DataDriven_TestNG_EXCEL/HRM_Login_Excel.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_27_DataDriven_TestNG_EXCEL/HRM_Login_Excel.java
@@ -2,6 +2,16 @@ package _S_05_27_DataDriven_TestNG_EXCEL;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Driver;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -9,13 +19,53 @@ import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class HRM_Login_Excel {
-
-	public void Get_Excel_Data() throws BiffException, IOException {
-		FileInputStream FileInputStream_Obj = new FileInputStream("C:\\WebDriver\\HRM Login.xls");
-		Workbook Workbook_obj = Workbook.getWorkbook(FileInputStream_Obj);
-		Sheet Sheet_Obj = Workbook_obj.getSheet(0);
-		int rows = Sheet_Obj.getRows();
-		int columns = Sheet_Obj.getColumns();
-		
+	String[][] data =	{
+			{"Admin","admin123"},
+			{"admin123","Admin"},
+			{"Admin123","admin123"},
+			{"Admin1","admin123"}
+		};
 	
-}}
+	@DataProvider(name ="LoginData")
+	public String[][] Login_Data_Provider(){
+	return data;
+	}
+	
+	@Test(dataProvider ="LoginData")
+	public void Login(String userName, String Password) {
+		WebDriver driver;
+		driver =new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		WebElement User = driver.findElement(By.xpath("//input[@name='username']"));
+		User.sendKeys(userName);
+		
+		WebElement Pass = driver.findElement(By.xpath("//input[@type='password']"));
+		Pass.sendKeys(Password);
+		
+		WebElement Button = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+		Button.click();
+			}
+	}
+
+	
+
+//	public void Get_Excel_Data() throws BiffException, IOException {
+//		FileInputStream FileInputStream_Obj = new FileInputStream("C:\\WebDriver\\HRM Login.xls");
+//		Workbook Workbook_obj = Workbook.getWorkbook(FileInputStream_Obj);
+//		Sheet Sheet_Obj = Workbook_obj.getSheet(0);
+//		int rows = Sheet_Obj.getRows();
+//		int columns = Sheet_Obj.getColumns();
+//
+//		String testDataString[][] = new String [rows-1][columns];
+//		for(int i =1;i<rows;i++) {
+//			for(int j =1;j<columns;j++) {
+//				testDataString[i-1][j] = Sheet_Obj.getCell(i,j).getContents();
+//			}
+//		}
+//		return testDataString;
+//		}
+	
+	
+	
+
