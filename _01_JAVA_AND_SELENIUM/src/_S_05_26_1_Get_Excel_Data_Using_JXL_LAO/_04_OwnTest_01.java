539f13c0c22391cdcd5bb749ac3031ea155bf20a
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2025-08-19 16:11:50 +0530

    SJ 19 AUG 25 FROM OFFICE

---
+++ _01_JAVA_AND_SELENIUM/src/_S_05_26_1_Get_Excel_Data_Using_JXL_LAO/_04_OwnTest_01.java
@@ -0,0 +1,33 @@
+package _S_05_26_1_Get_Excel_Data_Using_JXL_LAO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class _04_OwnTest_01 {
+
+	public static void main(String[] args) throws BiffException, IOException {
+	
+		FileInputStream FIS = new FileInputStream("C:\\WebDriver\\TESTING FILES\\OWN TEST JXL.xls");
+		Workbook wb_Obj = Workbook.getWorkbook(FIS);
+		Sheet sheet = wb_Obj.getSheet(0);
+		
+		int rows = sheet.getRows();
+		int columns = sheet.getColumns();
+		
+		System.out.println("rows ="+rows);
+		System.out.println("columns = "+columns);
+		
+		
+		String contents;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				contents = sheet.getCell(j, i).getContents();
+				System.out.println(contents + "\t");
+			} 
+		}			
+	}
+}
---
+++ _01_JAVA_AND_SELENIUM/src/_S_05_26_1_Get_Excel_Data_Using_JXL_LAO/_04_OwnTest_02.java
@@ -0,0 +1,65 @@
+package _S_05_26_1_Get_Excel_Data_Using_JXL_LAO;
+
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class _04_OwnTest_02 {
+
+	public static void main(String[] args) throws BiffException, IOException, InterruptedException {
+		 // Load Excel file
+        FileInputStream fis = new FileInputStream(new File("C:\\WebDriver\\TESTING FILES\\BRM Login1.xls"));
+        Workbook wb = Workbook.getWorkbook(fis);
+        Sheet sh = wb.getSheet(0);   // first sheet
+        
+        // Get total rows and cols
+        int rows = sh.getRows();
+        int cols = sh.getColumns();
+        
+        System.out.println("Total Rows: " + rows + " | Total Cols: " + cols);
+
+        // Launch Browser (Firefox example, set geckodriver path if needed)
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        // Example: read username and password from excel
+        String username = sh.getCell(0, 1).getContents(); // col=0,row=1
+        String password = sh.getCell(1, 1).getContents(); // col=1,row=1
+        
+        System.out.println("Username: " + username + " | Password: " + password);
+
+        // Open website and login (example: demo site)
+        driver.get("http://brm.tremplintech.in/web_pages/ord_reg.aspx");
+
+//        WebElement userField = driver.findElement(By.id("username"));
+//        WebElement passField = driver.findElement(By.id("password"));
+//        WebElement   Login_Button = driver.findElement(By.xpath("//*[@id=\"Button3\"]"));
+//		
+//        userField.sendKeys(username);
+//        passField.sendKeys(password);
+//        Login_Button.click();
+        
+        
+    	driver.navigate().to("http://brm.tremplintech.in/web_pages/ord_reg.aspx");
+		WebElement  User_Name = driver.findElement(By.xpath("//input[@name='txt_unam']"));
+		User_Name.sendKeys(username);
+		WebElement   Pass_Word = driver.findElement(By.xpath("//input[@name='txt_pass']"));
+		Pass_Word.sendKeys(password);
+		
+		Thread.sleep(5000);
+		WebElement   Login_Button = driver.findElement(By.xpath("//*[@id=\"Button3\"]"));
+		Login_Button.click();
+
+        // Close workbook
+        wb.close();
+    }
+}
